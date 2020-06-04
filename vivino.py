730776085d3218cb0e8af3809981bd630c264224
yuhao tan <tanyuhao@hotmail.com>
2020-06-04 12:41:03 +0800

    base template of scroll and record

--- vivino.py
+++ vivino.py
@@ -8,9 +8,28 @@ import time
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://www.vivino.com/explore?e=eJzLLbI1VMvNzLM1UMtNrLA1MzBQS660DXZXSwYSLmoFQNn0NNuyxKLM1JLEHLX8JNuixJLMvPTi-MSy1KLE9FS1fNuU1OJktfKS6FhbQwD0Vhp7")
-time.sleep(3)
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(3)
+
+def scroll_down():
+    """A method for scrolling the page."""
+    # Get scroll height.
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        # Scroll down to the bottom.
+        time.sleep(3)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Wait to load the page.
+        time.sleep(3)
+        # Calculate new scroll height and compare with last scroll height.
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+scroll_down()
+
+#time.sleep(3)
+#driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#time.sleep(3)
 
 winery = driver.find_elements_by_css_selector('span.vintageTitle__winery--2YoIr')
 winename = driver.find_elements_by_css_selector('span.vintageTitle__wine--U7t9G')
@@ -19,7 +38,7 @@ region = driver.find_elements_by_css_selector('div.vintageLocation__vintageLocat
 rating = driver.find_elements_by_css_selector('div.vivinoRatingWide__averageValue--1zL_5')
 numberOfRatings = driver.find_elements_by_css_selector('div.vivinoRatingWide__basedOn--s6y0t')
 
-for i in winery:
+for i in winename:
     if i == country:
         textname = i.text.split('\n')[0]
     elif i == region:
@@ -27,6 +46,5 @@ for i in winery:
     else:
         textname = i.text
     print(textname)
-
 
 driver.quit()
